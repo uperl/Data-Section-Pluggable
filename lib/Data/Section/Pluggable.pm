8644a6c5bd6583c2f796fbcc50b013196f50f9cd
Graham Ollis <plicease@cpan.org>
2024-12-04 10:47:13 -0700

    Initial structure

---
+++ lib/Data/Section/Pluggable.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package Data::Section::Pluggable {
+
+  # ABSTRACT: Read structured data from __DATA__
+}
+
+1;
+
---
+++ t/data_section_pluggable.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Data::Section::Pluggable;
+
+ok 1, 'todo';
+
+done_testing;
+
+
