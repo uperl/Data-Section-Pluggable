e7590d0f4d32f3a4857c075b7516cf178e61916e
Graham Ollis <plicease@cpan.org>
2024-12-04 22:14:12 -0700

    documentation for role

--- lib/Data/Section/Pluggable/Role/ContentProcessorPlugin.pm
+++ lib/Data/Section/Pluggable/Role/ContentProcessorPlugin.pm
@@ -10,13 +10,32 @@ package Data::Section::Pluggable::Role::ContentProcessorPlugin {
 
 =head1 SYNOPSIS
 
+# EXAMPLE: examples/content_processor_plugin.pl
+
 =head1 DESCRIPTION
+
+This plugin role provides a simple wrapper mechanism around
+the L<Data::Section::Pluggable> L<method add_format|/add_format>,
+making it an appropriate way to add such recipes to CPAN.
 
 =head1 METHODS
 
+All methods are to be implemented by your class.
+
 =head2 extensions
 
+ my @extensions = $plugin->extensions;
+ my \@extensions = $plugin->extensions;
+
+Returns a list or array reference of filename extensions the plugin
+should apply to.
+
 =head2 process_content
+
+ my $processed = $plugin->process_content($dps, $content);
+
+Takes the L<Data::Section::Pluggable> instance and content and returns
+the process content.
 
 =cut
 
